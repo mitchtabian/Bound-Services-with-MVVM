550d0553a1ce24b70c6aef2b80238e22cc839b2f
Mitch Tabian <mitch@tabian.ca>
2018-12-11 08:28:54 -0800

    starting point

--- app/src/main/java/com/codingwithmitch/boundserviceexample1/MainActivity.java
+++ app/src/main/java/com/codingwithmitch/boundserviceexample1/MainActivity.java
@@ -27,8 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     // Vars
-    private MyService mService;
-    private MainActivityViewModel mViewModel;
+    
 
 
     @Override
@@ -39,122 +38,18 @@ public class MainActivity extends AppCompatActivity {
         mTextView = findViewById(R.id.text_view);
         mButton = findViewById(R.id.toggle_updates);
 
-        mViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
-        setObservers();
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toggleUpdates();
-            }
-        });
-    }
 
-    private void toggleUpdates(){
-        if(mService != null){
-            if(mService.getProgress() == mService.getMaxValue()){
-                mService.resetTask();
-                mButton.setText("Start");
-            }
-            else{
-                if(mService.getIsPaused()){
-                    mService.unPausePretendLongRunningTask();
-                    mViewModel.setIsProgressBarUpdating(true);
-                }
-                else{
-                    mService.pausePretendLongRunningTask();
-                    mViewModel.setIsProgressBarUpdating(false);
-                }
-            }
-
-        }
-    }
-
-    private void setObservers(){
-        mViewModel.getBinder().observe(this, new Observer<MyService.MyBinder>() {
-            @Override
-            public void onChanged(@Nullable MyService.MyBinder myBinder) {
-                if(myBinder == null){
-                    Log.d(TAG, "onChanged: unbound from service");
-                }
-                else{
-                    Log.d(TAG, "onChanged: bound to service.");
-                    mService = myBinder.getService();
-                }
-            }
-        });
-
-        mViewModel.getIsProgressBarUpdating().observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(@Nullable final Boolean aBoolean) {
-                final Handler handler = new Handler();
-                final Runnable runnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        if(aBoolean){
-                            if(mViewModel.getBinder().getValue() != null){ // meaning the service is bound
-                                if(mService.getProgress() == mService.getMaxValue()){
-                                    mViewModel.setIsProgressBarUpdating(false);
-                                }
-                                mProgressBar.setProgress(mService.getProgress());
-                                mProgressBar.setMax(mService.getMaxValue());
-                                String progress =
-                                        String.valueOf(100 * mService.getProgress() / mService.getMaxValue()) + "%";
-                                mTextView.setText(progress);
-                            }
-                            handler.postDelayed(this, 100);
-                        }
-                        else{
-                            handler.removeCallbacks(this);
-                        }
-                    }
-                };
-
-                // control what the button shows
-                if(aBoolean){
-                    mButton.setText("Pause");
-                    handler.postDelayed(runnable, 100);
-
-                }
-                else{
-                    if(mService.getProgress() == mService.getMaxValue()){
-                        mButton.setText("Restart");
-                    }
-                    else{
-                        mButton.setText("Start");
-                    }
-                }
             }
         });
     }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        startService();
-    }
 
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if(mViewModel.getBinder() != null){
-            unbindService(mViewModel.getServiceConnection());
-        }
-    }
-
-    private void startService(){
-        Intent serviceIntent = new Intent(this, MyService.class);
-        startService(serviceIntent);
-
-        bindService();
-    }
-
-    private void bindService(){
-        Intent serviceBindIntent =  new Intent(this, MyService.class);
-        bindService(serviceBindIntent, mViewModel.getServiceConnection(), Context.BIND_AUTO_CREATE);
-    }
 
 
 }
